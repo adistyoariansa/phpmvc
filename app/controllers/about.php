7b8fbe1877cc63728618450afac10a6bc0e7bba1
adistyoariansa <adistyoariansa2@gmail.com>
2024-10-25 00:50:07 +0700

    first commit

---
+++ app/controllers/about.php
@@ -0,0 +1,23 @@
+<?php
+
+class About extends controller {
+    public function index($nama = 'adistyo', $pekerjaan = 'website conten', $umur = 20)
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+    }
+    public function page()
+    {
+        $data['judul'] = 'pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+    }
+}
+
+?>
